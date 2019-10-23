467e68f48415cd1189f65d7c4cf348400d638c24
Marine27 <52531382+Marine27@users.noreply.github.com>
2019-10-23 16:11:28 +0200

    Update index.js

--- source1/index.js
+++ source1/index.js
@@ -1,6 +1,7 @@
 
 
-var result;
+var result_monney;
+var result_pays;
 var url_monney = "https://api.exchangeratesapi.io/latest";
 var url_pays = "https://restcountries.eu/rest/v2/all";
 
@@ -10,18 +11,18 @@ fetch(url_monney)
 .then(function(response){
   response.json()
   .then(function(data){
-    result = data;
-    console.log(result);
+    result_monney = data;
+    console.log(result_monney);
 
   })
 })
 
-fetch(url_pays)
+fetch(url__pays)
 .then(function(response){
   response.json()
   .then(function(data){
-    result = data;
-    console.log(result);
+    result_pays = data;
+    console.log(result_pays);
 
   })
 })
