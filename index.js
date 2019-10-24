dcaa6668c888a33b18b9df3082d6b32e0475bf0b
Marwa Elatrache <marwa-el2@hotmail.com>
2019-10-24 15:52:02 +0200

    ajout des lignes

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
-var result_monney;
-var result_pays;
-var url_monney = "https://api.exchangeratesapi.io/latest";
-var url_pays = "https://restcountries.eu/rest/v2/all";
+var resu_taux;
+var resu_pays;
+var url_taux = "https://api.exchangeratesapi.io/latest";
+var url_pays = "https://restcountries.eu/rest/v2/name/";
+
+liste_pays = ["germany", "france"];
+
+//console.log("Hi");
 
 
 
-fetch(url_monney)
+
+
+fetch(url_taux)
   .then(function(response) {
     response.json()
       .then(function(data) {
-        result_monney = data;
-        console.log(result_monney);
-
+        resu_taux = data;
+        //console.log(resu_taux.rates);
       })
   })
 
-fetch(url_pays)
-  .then(function(response) {
-    response.json()
-      .then(function(data) {
-        result_pays = data;
-        console.log(result_pays);
 
+
+//var nom = document.getElementById("capital").innerText;
+var lien = null;
+
+//console.log(nom);
+
+var tableau = document.getElementById("tableau");
+console.log("tableau", tableau);
+
+
+
+
+  for(i=0; i<liste_pays.length;i++){
+    fetch(url_pays + liste_pays[i])
+      .then(function(response) {
+        response.json()
+          .then(function(data) {
+            resu_pays = data;
+            var row = tableau.insertRow(1);
+            var c1 = row.insertCell(0);
+            var c2 = row.insertCell(1);
+            var c3 = row.insertCell(2);
+            var c4 = row.insertCell(3);
+            var c5 = row.insertCell(4);
+            var c6 = row.insertCell(5);
+            c1.innerText =resu_pays[0].name;
+          })
       })
-  })
+  }
+
+
+
+
+  //console.log(resu_pays);
+//  console.log(resu_pays[0].name);
+  //console.log(nom);
+//  document.getElementById("pays").innerText = resu_pays[0].name;
